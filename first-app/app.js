ff983ac8ae6ffdadc17b100b42d78f3ee276d3e2
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-14 18:13:58 +0100

    Passing arguments with an event; JS arrow-function

--- first-app/app.js
+++ first-app/app.js
@@ -1,8 +1,8 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log('A new message has been logged');
-})
+emitter.on('messageLogged', (arg) => {
+    console.log('A new message has been logged', arg);
+});
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
