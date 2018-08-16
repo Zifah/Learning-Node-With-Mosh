284f6fdbd2596b778ca513317f15bf7ae768c186
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-16 15:51:01 +0100

    Write code that uses my own package

--- first-app/app.js
+++ first-app/app.js
@@ -1,16 +1,2 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.write('Hello World!');
-        res.end();
-    }
-
-    if(req.url === '/api/courses'){
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
-});
-
-server.listen(3000);
-console.log("Listening on port 3000...")
+var lionLib = require('lion-lib-hafiz');
+console.log(lionLib.add(1, 2));
