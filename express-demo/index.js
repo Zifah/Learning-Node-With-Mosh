e761c1338ec6f95be13f7001a330baaae262ed92
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-17 17:21:02 +0100

    Create API endpoints using the Express package

---
+++ express-demo/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
