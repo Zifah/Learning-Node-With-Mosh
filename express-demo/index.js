fbe0c3d6f90059781c66c3a1b19a306d80db1b28
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-17 23:05:29 +0100

    Accessing environment variables via process.ENV

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,4 +9,5 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
