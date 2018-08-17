1a204f4eeec242596765c8a6da521dd1fb8880ca
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-17 17:24:42 +0100

    Using nodemon to automatically restart express web server when source is edited

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
