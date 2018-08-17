5926070191798070c4243d7e0749237be49a104d
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-17 23:08:37 +0100

    Add a route parameter

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,5 +9,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
