8eef8327b19dd960e3b5da7f32d06ff937c5d398
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-18 23:18:05 +0100

    Refactor input validation into a new function

--- express-demo/index.js
+++ express-demo/index.js
@@ -25,10 +25,10 @@ app.post('/api/courses', (req, res) => {
     const schema = {
         name: Joi.string().min(3).required()
     };
-
-    const validity = Joi.validate(req.body, schema);
-    if(validity.error){
-        res.status(400).send(validity.error.details[0].message);
+    
+    const { error } = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -51,15 +51,23 @@ app.put('/api/courses/:id', (req, res) => {
         name: Joi.string().min(3).required()
     };
 
-    const validity = Joi.validate(req.body, schema);
-    if(validity.error){
-        res.status(400).send(validity.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
         return;
     }
 
     course.name = req.body.name;
     res.send(course);
 });
+
+function validateCourse(course){    
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
