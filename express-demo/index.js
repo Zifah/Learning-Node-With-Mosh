3d040942f0cad0d15c5f54b1dfe9c819d80c5721
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-20 08:30:41 +0100

    Using other middleware built into express

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 const courses = [{ id: 1, name: 'Mathematics' }, { id: 2, name: 'English' }, 
 { id: 3, name: 'Yoruba' }];
