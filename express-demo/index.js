0ea2fbe98333222ad00189c8a1017f3d3c546e0a
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-21 00:29:32 +0100

    Add in and apply a configuration package named "config"

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
@@ -8,6 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+
+console.log('Application Name:', config.get('name'));
+console.log('Mail Server:', config.get('mail.host'));
+console.log('Mail Password:', config.get('mail.password'));
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
