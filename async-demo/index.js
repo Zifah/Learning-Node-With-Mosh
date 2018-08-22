4c4f03a8b7935e81d5f2dc89931da022e55c0706
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-22 07:06:33 +0100

    Move asynchronous code-block out to its own function

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,11 @@
 console.log('Before');
-
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000);
-
+const user = getUser(1);
+console.log(user);
 console.log('After');
+
+function getUser(id){
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        return { id: id, gitHuibUsername: 'Zifah'}
+    }, 2000);
+}
