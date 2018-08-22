8e788d1d5e591d31c3b0d5e8cb3c6aeff8ff0003
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-22 07:25:19 +0100

    Using a callback with an async method

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,12 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+    console.log(user);
+});
 console.log('After');
 
-function getUser(id){
+function getUser(id, callback){
     setTimeout(() => {
         console.log('Reading a user from a database...');
-        return { id: id, gitHuibUsername: 'Zifah'}
+        callback({ id: id, gitHubUsername: 'Zifah'});
     }, 2000);
 }
