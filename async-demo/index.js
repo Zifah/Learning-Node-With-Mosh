84e750755d69b48bba0a144283e0c1d2dbe69097
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-22 07:48:11 +0100

    Create another async method to get a GitHub user's repositories

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,9 @@
 console.log('Before');
 getUser(1, (user) => {
     console.log(user);
+    getRepositories(user.gitHubUsername, (repositories) => {
+        console.log(`${user.gitHubUsername}'s repositories: `, repositories);
+    });
 });
 console.log('After');
 
@@ -8,5 +11,12 @@ function getUser(id, callback){
     setTimeout(() => {
         console.log('Reading a user from a database...');
         callback({ id: id, gitHubUsername: 'Zifah'});
+    }, 2000);
+}
+
+function getRepositories(username, callback){
+    setTimeout(() => {
+        console.log(`Reading ${username}'s repositories from GitHub API`);
+        callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
