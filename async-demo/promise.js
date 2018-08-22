dcb04e2edece83e0b45ad86e8d477ac66a818e8d
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-22 19:26:12 +0100

    Incorporate error-handling for a rejected promise

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -3,7 +3,10 @@ const p = new Promise((resolve, reject) => {
     // ...
     setTimeout(() => {
         resolve(1);
+        // reject(new Error('message'));
     }, 2000);
 });
 
-p.then(result => console.log('Result: ', result));
+p
+  .then(result => console.log('Result: ', result))
+  .catch(err => console.log('Error:', err));
