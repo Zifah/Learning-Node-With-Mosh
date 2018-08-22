46ecd252a2c8cd78e7ccf71fd82b8aef0bb2266b
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-22 21:11:48 +0100

    Replace callbacks with Promises to improve code quality

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,26 +1,34 @@
 console.log('Before');
-getUser(1, displayUser);
+getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repositories => getCommits(repositories[0]))
+    .then(commits => console.log('Commits:', commits))
+    .catch(err => console.log('Error:', err.message));
 console.log('After');
 
-function displayRepositories(username, repositories){
-    console.log(`${username}'s repositories: `, repositories);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'Zifah' });
+        }, 2000);
+    });
 }
 
-function displayUser(user){
-    console.log(user);
-    getRepositories(user.gitHubUsername, displayRepositories);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Reading ${username}'s repositories from GitHub API`);
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
 }
 
-function getUser(id, callback){
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({ id: id, gitHubUsername: 'Zifah'});
-    }, 2000);
-}
-
-function getRepositories(username, callback){
-    setTimeout(() => {
-        console.log(`Reading ${username}'s repositories from GitHub API`);
-        callback(username, ['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getCommits(repo){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Fetching GitHub commits on repo: ${repo}`);
+            resolve([ 'commit1' ]);
+        }, 2000);
+    });
 }
