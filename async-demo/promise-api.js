791b8801fe6b9a9b38024ebb9bfacf47723eaec4
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-23 06:21:26 +0100

    Creare a rejected Promise

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,3 +1,5 @@
-const p = Promise.resolve({ id: 1});
+const p = Promise.reject(new Error("Promise Rejected!"));
 
-p.then(result => console.log(result));
+p
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
