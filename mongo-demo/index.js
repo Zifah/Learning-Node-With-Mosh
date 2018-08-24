b033f993a9681cea51830a184cc90a5079d7c038
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-24 09:54:32 +0100

    Create a schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,11 @@ mongoose
     .connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.log('Could not connect to MongoDB...', e));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now},
+    isPublished: Boolean
+});
