0a9cff8eeaa7561673e7d1b1bd989d47e219c4dd
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-08-31 10:18:06 +0100

    Remove unused field 'id' from genres database initialization array

--- vidly/bootstrap.js
+++ vidly/bootstrap.js
@@ -1,8 +1,8 @@
 const genres = require('./routes/genres');
 
 const genreList = [
-    { id: 1, name: 'Thriller', description: 'Movies that increase the heart rate' },
-    { id: 2, name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears' }
+    { name: 'Thriller', description: 'Movies that increase the heart rate' },
+    { name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears' }
 ];
 
 for(var i = 0; i < genreList.length; i++){
