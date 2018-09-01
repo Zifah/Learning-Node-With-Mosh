ac90053ca704a9c8d4e923133e2bd9185df487ab
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-01 20:54:46 +0100

    Create an object reference

---
+++ mongo-demo/population.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const Author = mongoose.model('Author', new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+}));
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+}));
+
+async function createAuthor(name, bio, website) { 
+  const author = new Author({
+    name, 
+    bio, 
+    website 
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course
+    .find()
+    .select('name');
+  console.log(courses);
+}
+
+// createAuthor('Mosh', 'My bio', 'My Website');
+
+// createCourse('Node Course', '5b8ae88f67041a904c35f8c7')
+
+listCourses();
