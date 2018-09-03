7173040fb9271de4e1d4449e99500fc07dee7014
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-03 20:00:24 +0100

    Include file in project to demonstrate document embedding

---
+++ mongo-demo/embedding.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+createCourse('Node Course', new Author({ name: 'Mosh' }));
