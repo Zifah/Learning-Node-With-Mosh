68e1b90b8cee0a72871b97528487ff86bc907d4e
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-06 20:03:43 +0100

    Use Joi's e-mail validation method to valid new user e-mail

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -38,7 +38,8 @@ function validateUser(user) {
     email: Joi.string()
       .min(3)
       .max(255)
-      .required(),
+      .required()
+      .email(),
     password: Joi.string()
       .min(6)
       .max(100)
