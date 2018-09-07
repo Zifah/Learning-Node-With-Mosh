12e59ad6edc18a80b0beab854830516e074f9f9a
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-07 19:14:46 +0100

    Centralize error handling for all Genres module endpoints

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -36,107 +36,81 @@ router.get(
   })
 );
 
-router.get("/:id", async (req, res) => {
-  try {
+router.get(
+  "/:id",
+  asyncMiddleware(async (req, res) => {
     const genre = await Genres.findById(req.params.id);
     if (!genre)
       return res
         .status(404)
         .send(`A genre with id ${req.params.id} was not found!`);
     res.send(genre);
-  } catch (ex) {
-    logServerErrorAndRespond(
-      err,
-      `Error fetching genre with id: ${req.params.id}`,
-      res
-    );
-  }
-});
+  })
+);
 
-router.delete("/:id", [auth, admin], (req, res) => {
-  Genres.findByIdAndRemove(req.params.id)
-    .then(genre => {
-      if (!genre)
-        return res
-          .status(404)
-          .send(`A genre with id ${req.params.id} was not found!`);
-      res.send(genre);
-    })
-    .catch(err => {
-      logServerErrorAndRespond(
-        err,
-        `Error trying to delete genre with id: ${req.params.id}`,
-        res
-      );
-    });
-});
+router.delete(
+  "/:id",
+  [auth, admin],
+  asyncMiddleware(async (req, res) => {
+    const genre = await Genres.findByIdAndRemove(req.params.id);
+    if (!genre)
+      return res
+        .status(404)
+        .send(`A genre with id ${req.params.id} was not found!`);
+    res.send(genre);
+  })
+);
 
 function logServerErrorAndRespond(err, friendlyMessage, res, statusCode = 500) {
   console.log(friendlyMessage, err.message);
   res.status(statusCode).send(friendlyMessage);
 }
 
-router.put("/:id", auth, (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put(
+  "/:id",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const matchedGenre = await Genres.find({ name: req.body.name });
 
-  Genres.find({ name: req.body.name })
-    .then(matchedGenre => {
-      if (
-        matchedGenre &&
-        matchedGenre.length > 0 &&
-        matchedGenre[0]._id != req.params.id
-      )
-        return res
-          .status(400)
-          .send("Another genre with this name already exists");
+    if (
+      matchedGenre &&
+      matchedGenre.length > 0 &&
+      matchedGenre[0]._id != req.params.id
+    )
+      return res
+        .status(400)
+        .send("Another genre with this name already exists");
 
-      updateGenre(req.params.id, req.body)
-        .then(updated => {
-          if (!updated)
-            return res
-              .status(404)
-              .send(`A genre with id ${req.params.id} was not found!`);
-          res.send(updated);
-        })
-        .catch(err => {
-          logServerErrorAndRespond(
-            err,
-            `Error trying to update genre with id: ${req.params.id}`,
-            res
-          );
-        });
-    })
-    .catch(err => {
-      logServerErrorAndRespond(err, `Error trying to update genre`, res);
-    });
+    const updated = await updateGenre(req.params.id, req.body);
+    if (!updated)
+      return res
+        .status(404)
+        .send(`A genre with id ${req.params.id} was not found!`);
+    res.send(updated);
+    console.log(`Genre ${req.params.id} updated successfully`);
+  })
+);
 
-  console.log(`Genre ${req.params.id} updated successfully`);
-});
+router.post(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-router.post("/", auth, (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    const matchedGenre = await Genres.find({ name: req.body.name });
 
-  Genres.find({ name: req.body.name })
-    .then(matchedGenre => {
-      if (matchedGenre && matchedGenre.length > 0)
-        return res
-          .status(400)
-          .send("Another genre with this name already exists");
+    if (matchedGenre && matchedGenre.length > 0)
+      return res
+        .status(400)
+        .send("Another genre with this name already exists");
 
-      createGenre(req.body)
-        .then(newGenre => {
-          res.send(newGenre);
-        })
-        .catch(err => {
-          logServerErrorAndRespond(err, `Error trying to create genre`, res);
-        });
-    })
-    .catch(err => {
-      logServerErrorAndRespond(err, `Error trying to create genre`, res);
-    });
-});
+    const newGenre = await createGenre(req.body);
+    res.send(newGenre);
+  })
+);
 
 module.exports = {
   router: router,
