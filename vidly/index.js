609a133113a735812ee231a9bdbe4527e71d11cc
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-07 23:27:42 +0100

    Set lowest logging level to info

--- vidly/index.js
+++ vidly/index.js
@@ -22,7 +22,10 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, { db: "mongodb://localhost/vidly" });
+winston.add(winston.transports.MongoDB, {
+  db: "mongodb://localhost/vidly",
+  level: "info"
+});
 
 const app = express();
 app.use(express.json());
