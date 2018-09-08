c859c0cce3066072675bb1cf34de6300a10c64ce
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 15:23:51 +0100

    Group tests for the same unit using the Jest describe utility

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require("../lib");
 
-test("absolute - should return a positive number if input is positive", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe("absolute", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return a positive number if input is negative", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return zero if input is zero", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  it("should return zero if input is zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
