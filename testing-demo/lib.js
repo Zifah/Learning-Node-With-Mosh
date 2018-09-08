693077dd0ab5c93cac7e1872cee52434eae62f73
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 15:28:37 +0100

    Refactor code to demonstrate that unit test is not implementation dependent

--- testing-demo/lib.js
+++ testing-demo/lib.js
@@ -1,46 +1,43 @@
-const db = require('./db');
-const mail = require('./mail');
+const db = require("./db");
+const mail = require("./mail");
 
-// Testing numbers 
+// Testing numbers
 module.exports.absolute = function(number) {
-  if (number > 0) return number; 
-  if (number < 0) return -number; 
-  return 0; 
-}
+  return number >= 0 ? number : -number;
+};
 
-// Testing strings 
-module.exports.greet = function(name) { 
-  return 'Welcome ' + name; 
-}
+// Testing strings
+module.exports.greet = function(name) {
+  return "Welcome " + name;
+};
 
-// Testing arrays 
-module.exports.getCurrencies = function() { 
-  return ['USD', 'AUD', 'EUR'];
-}
+// Testing arrays
+module.exports.getCurrencies = function() {
+  return ["USD", "AUD", "EUR"];
+};
 
-// Testing objects 
-module.exports.getProduct = function(productId) { 
+// Testing objects
+module.exports.getProduct = function(productId) {
   return { id: productId, price: 10 };
-}
+};
 
-// Testing exceptions 
-module.exports.registerUser = function(username) { 
-  if (!username) throw new Error('Username is required.');
+// Testing exceptions
+module.exports.registerUser = function(username) {
+  if (!username) throw new Error("Username is required.");
 
-  return { id: new Date().getTime(), username: username }
-}
+  return { id: new Date().getTime(), username: username };
+};
 
-// Mock functions 
-module.exports.applyDiscount = function(order) { 
+// Mock functions
+module.exports.applyDiscount = function(order) {
   const customer = db.getCustomerSync(order.customerId);
 
-  if (customer.points > 10) 
-    order.totalPrice *= 0.9; 
-}
+  if (customer.points > 10) order.totalPrice *= 0.9;
+};
 
-// Mock functions 
-module.exports.notifyCustomer = function(order) { 
+// Mock functions
+module.exports.notifyCustomer = function(order) {
   const customer = db.getCustomerSync(order.customerId);
 
-  mail.send(customer.email, 'Your order was placed successfully.');
-}
+  mail.send(customer.email, "Your order was placed successfully.");
+};
