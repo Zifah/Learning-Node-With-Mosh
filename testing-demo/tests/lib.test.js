fa4073859f138f407670544e109334c6fb5d1b54
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 21:48:58 +0100

    Interaction based testing using manual approach

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require("../lib");
 const db = require("../db");
+const mail = require("../mail");
 
 describe("absolute", () => {
   it("should return a positive number if input is positive", () => {
@@ -70,5 +71,22 @@ describe("applyDiscount", () => {
     const order = { customerId: 1, totalPrice: 10 };
     lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = function(customerId) {
+      console.log("Fake reading customer...");
+      return { id: customerId, email: "a" };
+    };
+
+    let mailSent = false;
+    mail.send = function(email, message) {
+      mailSent = true;
+    };
+
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mailSent).toBe(true);
   });
 });
