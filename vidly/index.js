d23cf743044686dbe848a209e680abf39d19ec92
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 01:28:39 +0100

    Extract validation logic into its own module

--- vidly/index.js
+++ vidly/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const logging = require("./startup/logging")();
 const config = require("./startup/config")();
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-
 require("./startup/database")();
 
 const app = express();
 require("./startup/routes")(app);
+require("./startup/validation");
 
 const port = process.env.PORT_NUMBER || 3000;
 app.listen(port, () => console.log("Listening on port", port, "..."));
---
+++ vidly/startup/validation.js
@@ -0,0 +1,2 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
