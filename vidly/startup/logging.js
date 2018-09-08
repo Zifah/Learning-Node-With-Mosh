27e8771dfd11738e53430758adb2fe04d5aef30f
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 01:39:19 +0100

    Add in Console logging feature for unhandled exceptions

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -4,7 +4,8 @@ require("winston-mongodb");
 
 module.exports = function() {
   winston.handleExceptions(
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+    new winston.transports.File({ filename: "uncaughtExceptions.log" }),
+    new winston.transports.Console({ colorize: true, prettyPrint: true })
   );
 
   process.on("unhandledRejection", ex => {
