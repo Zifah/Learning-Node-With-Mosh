986f622e7e509168fc8bd4b53610d153e10fd7f4
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-08 15:19:16 +0100

    Write unit tests to cover a function that should always return the absolute equivalent of the input number

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,1 +1,16 @@
-test("Our first test", () => {});
+const lib = require("../lib");
+
+test("absolute - should return a positive number if input is positive", () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return a positive number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return zero if input is zero", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
+});
