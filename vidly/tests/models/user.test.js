d1ea9edcc830ea85bac41575b291164ab7c07a03
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-09 11:12:21 +0100

    Refactor unit test based on method used in the tutorial

---
+++ vidly/tests/models/user.test.js
@@ -0,0 +1,17 @@
+const { User } = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const config = require("config");
+
+describe("generateAuthToken", () => {
+  it("should return a JWT signed with a private key", () => {
+    const userSchema = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+    const userModel = new User(userSchema);
+    const token = userModel.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(userSchema);
+  });
+});
