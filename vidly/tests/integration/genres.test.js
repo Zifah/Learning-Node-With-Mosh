a072573e5a02afe67b9202e2b63a36576f3feb0c
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-10 20:09:03 +0100

    Write integration test for the GetGenreById route

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 let server;
 const request = require("supertest");
 const { Genres } = require("../../models/genre");
+const mongoose = require("mongoose");
 
 describe("/api/genres", () => {
   beforeEach(() => {
@@ -23,6 +24,24 @@ describe("/api/genres", () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some(g => g.name === "genre1")).toBeTruthy();
       expect(res.body.some(g => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return the genre for a valid id", async () => {
+      const theGenre = await new Genres({
+        name: "genre1"
+      }).save();
+
+      const res = await request(server).get(`/api/genres/${theGenre._id}`);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("_id", theGenre._id.toHexString());
+      expect(res.body).toHaveProperty("name", "genre1");
+    });
+
+    it("should return error 404 for an invalid id", async () => {
+      const res = await request(server).get(`/api/genres/1`);
+      expect(res.status).toBe(404);
     });
   });
 });
