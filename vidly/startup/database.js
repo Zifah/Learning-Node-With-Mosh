e74d2b5430803b02701af39c1cc8fd25c6c714b2
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-10 19:05:59 +0100

    Configurize the database connection string

--- vidly/startup/database.js
+++ vidly/startup/database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 
 module.exports = function connectToDatabase() {
+  const db = config.get("db");
   mongoose
-    .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
-    .then(() => winston.info("Connected to MongoDB successfully..."));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => winston.info(`Connected to ${db} successfully...`));
 };
