c65147d6be22b3e83dca043597e7fe936d0db24c
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-10 20:40:34 +0100

    Write the integration tests suite for the create Genre route; fix some validation bugs

--- vidly/models/genre.js
+++ vidly/models/genre.js
@@ -1,40 +1,43 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const genreSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: false,
-        minlength: 10,
-        maxlength: 250,
-        trim: true
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: false,
+    minlength: 10,
+    maxlength: 250,
+    trim: true
+  }
 });
 
 function getGenresModel() {
-    return mongoose.model('Genres', genreSchema);
+  return mongoose.model("Genres", genreSchema);
 }
 
 const Genres = getGenresModel();
 
 function validateGenre(genre) {
-    const schema = {
-        name: Joi.string().min(3).required(),
-        description: Joi.string()
-    }
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    description: Joi.string()
+  };
 
-    return Joi.validate(genre, schema);
+  return Joi.validate(genre, schema);
 }
 
 module.exports = {
-    Genres: Genres,
-    validate: validateGenre,
-    schema: genreSchema
+  Genres: Genres,
+  validate: validateGenre,
+  schema: genreSchema
 };
--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -2,6 +2,7 @@ let server;
 const request = require("supertest");
 const { Genres } = require("../../models/genre");
 const mongoose = require("mongoose");
+const { User } = require("../../models/user");
 
 describe("/api/genres", () => {
   beforeEach(() => {
@@ -42,6 +43,33 @@ describe("/api/genres", () => {
     it("should return error 404 for an invalid id", async () => {
       const res = await request(server).get(`/api/genres/1`);
       expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST /", () => {
+    it("should return error 401 if client is not logged in", async () => {
+      const res = await request(server)
+        .post(`/api/genres`)
+        .send({ name: "genre1" });
+      expect(res.status).toBe(401);
+    });
+
+    it("should return error 400 if genre name is less than 3 characters", async () => {
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .post(`/api/genres`)
+        .set("x-auth-token", token)
+        .send({ name: "12" });
+      expect(res.status).toBe(400);
+    });
+
+    it("should return error 400 if genre name is more than 50 characters", async () => {
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .post(`/api/genres`)
+        .set("x-auth-token", token)
+        .send({ name: new Array(52).join("1") });
+      expect(res.status).toBe(400);
     });
   });
 });
