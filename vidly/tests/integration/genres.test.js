d342f720c989ef213481dd0c2d14235ca0af94c1
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-10 19:35:48 +0100

    Write a more specific integration test for the GET/ genres endpoint

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,18 +1,28 @@
 let server;
 const request = require("supertest");
+const { Genres } = require("../../models/genre");
 
 describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genres.remove({});
   });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
+      await Genres.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" }
+      ]);
+
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some(g => g.name === "genre2")).toBeTruthy();
     });
   });
 });
