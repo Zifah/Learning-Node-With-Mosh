b1dd333ed1c7dab0548df67713d279d4589ed184
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-11 22:10:39 +0100

    Fix some testing issues

--- vidly/tests/integration/auth.test.js
+++ vidly/tests/integration/auth.test.js
@@ -1,14 +1,17 @@
 let server;
 const request = require("supertest");
 const { User } = require("../../models/user");
+const { Genres } = require("../../models/genre");
 const mongoose = require("mongoose");
 
-describe("auth middleware", () => {
+describe("auth middleware", async () => {
   beforeEach(() => {
     server = require("../../index");
     token = new User().generateAuthToken();
   });
-  afterEach(() => {
+
+  afterEach(async () => {
+    await Genres.deleteMany({});
     server.close();
   });
 
@@ -29,5 +32,16 @@ describe("auth middleware", () => {
     token = "";
     const res = await exec();
     expect(res.status).toBe(401);
+  });
+
+  it("should return 400 if token is invalid", async () => {
+    token = "a";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 200 status if token is valid", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
   });
 });
